e038732ef5b0dabd420742cb7e24fb8f99cc2d13
vishalkakadiya <vishalkakadiya123@gmail.com>
2014-12-02 15:51:30 +0530

    Update unlink_directory.php

--- libs/unlink_directory.php
+++ libs/unlink_directory.php
@@ -5,10 +5,10 @@ class unlink_directory {
     function remove_directory($directory) {
         foreach (glob("{$directory}/*") as $file) {
             if ( is_dir( $file ) ) { 
-                remove_directory($file);
-        		} else {
-        			unlink($file);
-        		}
+                $this->remove_directory($file);
+    		} else {
+    			unlink($file);
+    		}
         }
         rmdir($directory);
     }
