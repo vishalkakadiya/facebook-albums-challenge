6c106963c37d59a22dc197f6fd56fa1197553123
vishalkakadiya <vishalkakadiya123@gmail.com>
2014-12-02 16:20:45 +0530

    Create unlink_directoryTest.php

---
+++ Tests/unlink_directoryTest.php
@@ -0,0 +1,23 @@
+
+<?php
+
+class unlink_directoryTest extends PHPUnit_Framework_TestCase {
+
+    protected $unlink_directory;
+
+    protected function setUp() {
+        $this->unlink_directory = new unlink_directory();
+    }
+
+    function testremove_directory( $directory = null) {
+        $actual = $this->zipper->remove_directory( $directory );
+        $this->assertEquals( $actual, $actual );
+    }
+
+    protected function tearDown() {
+        unset($this->zipper);
+    }
+
+}
+
+?>
